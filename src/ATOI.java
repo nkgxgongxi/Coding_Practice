1d963883b5c5c5b011570ac526615587ed1fe1b0
nkgxgongxi <nkgxgongxi@gmail.com>
2013-09-06 14:28:29 -0400

    9/6 v4

--- src/ATOI.java
+++ src/ATOI.java
@@ -1,8 +1,10 @@
-
 public class ATOI {
 	public int atoi(String s) {
-        int res = 0;
+        long res = 0;
 		boolean isNegative = false;
+		while(s.length() > 0 && s.startsWith(" ")){
+			s = s.substring(1);
+		}
         if(s.length() == 0)
             return 0;
 		if(s.startsWith("-")){
@@ -18,9 +20,16 @@ public class ATOI {
 				return 0;
 			}
 		}
-		int counter = s.length() - 1;
+		int len = 0;
 		for(int i = 0; i < s.length(); i++){
-			if(s.charAt(0) < '0' || s.charAt(0) > '9'){
+			if(s.charAt(i) >= '0' && s.charAt(i) <= '9')
+				len ++;
+			else
+				break;
+		}
+		int counter = len - 1;
+		for(int i = 0; i < len && len != 0; i++){
+			if(s.charAt(i) < '0' || s.charAt(i) > '9'){
 				return 0;
 			}
 //			System.out.println(res);
@@ -29,20 +38,20 @@ public class ATOI {
 //			System.out.println(res);
 		}
 		if(isNegative){
-			if(res >= Integer.MAX_VALUE)
+			if(res > Integer.MAX_VALUE)
 				return Integer.MIN_VALUE;
 			else
-				return (0 - res);
+				return (0 - (int) res);
 		}
 		else{
-			if(res >= Integer.MAX_VALUE)
+			if(res > Integer.MAX_VALUE)
 				return Integer.MAX_VALUE;
 			else
-				return res;
+				return (int) res;
 		}
 	}
 	public static void main(String[] args){
-		String input = "+1";
+		String input = " b11228552307";
 		System.out.println(new ATOI().atoi(input));
 	}
 }
